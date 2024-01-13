8c65745740585ebe69b6407c73c8a91b7c52dc91
siloksan <tutel07@gmail.com>
2024-01-13 19:20:47 +0300

    feat: implement write file

---
+++ 02-write-file/index.js
@@ -0,0 +1,27 @@
+require('colors');
+const readline = require('readline');
+const fs = require('fs');
+const { stdin: input, stdout: output } = require('process');
+const path = require('path');
+
+const rl = readline.createInterface({ input, output });
+const pathToFile = path.join('02-write-file', 'text.txt');
+const writeStream = fs.createWriteStream(pathToFile);
+
+function question() {
+  const exit = 'exit'.red;
+  output.write(
+    `Do you want to add something else, if not, please enter '${exit}'?\n`,
+  );
+}
+
+question();
+
+rl.on('line', (input) => {
+  if (input === 'exit') {
+    rl.close();
+  } else {
+    writeStream.write(input);
+    question();
+  }
+});
