5bff1936e3cf6ef6ae09347977297f5545e20e7d
siloksan <tutel07@gmail.com>
2024-01-14 21:47:01 +0300

    feat: implement files in folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,27 @@
+const { readdir, stat } = require('fs');
+const path = require('path');
+
+const pathToFolder = path.join('03-files-in-folder', 'secret-folder');
+
+readdir(pathToFolder, { withFileTypes: true }, (err, files) => {
+  if (err) {
+    console.log(err);
+  } else {
+    files.forEach((file) => {
+      if (file.isFile()) {
+        const pathToFile = path.join(pathToFolder, file.name);
+        stat(pathToFile, (err, stats) => {
+          if (err) {
+            console.log(err);
+          } else {
+            const name = file.name.split('.')[0];
+            const extension = path.extname(file.name).slice(1);
+            const size = stats.size;
+            const fileInfo = `${name} - ${extension} - ${size} bytes`;
+            console.log(fileInfo);
+          }
+        });
+      }
+    });
+  }
+});
