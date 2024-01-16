8f82b63957352798df53865c303028e6ef41588f
siloksan <tutel07@gmail.com>
2024-01-16 11:51:32 +0300

    feat: implement copy directory

---
+++ 04-copy-directory/index.js
@@ -0,0 +1,55 @@
+const { readdir, copyFile, mkdir, stat, unlink } = require('fs');
+const path = require('path');
+
+const pathToFolder = path.join('04-copy-directory', 'files');
+const pathToCopyFolder = path.join('04-copy-directory', 'files-copy');
+
+stat(pathToCopyFolder, (err, stats) => {
+  if (err) {
+    return;
+  } else {
+    if (stats.isDirectory()) {
+      readdir(pathToCopyFolder, { withFileTypes: true }, (err, files) => {
+        if (err) {
+          console.log(err);
+        } else {
+          if (files.length === 0) return;
+          files.forEach((file) => {
+            const pathToFile = path.join(pathToCopyFolder, file.name);
+            unlink(pathToFile, (err) => {
+              if (err) console.log(err);
+            });
+          });
+        }
+      });
+    }
+  }
+});
+
+mkdir(pathToCopyFolder, { recursive: true }, (err) => {
+  if (err) console.log(err);
+  return;
+});
+
+stat(pathToFolder, (err, stats) => {
+  if (err) {
+    console.log(err);
+  } else {
+    if (stats.isDirectory()) {
+      readdir(pathToFolder, { withFileTypes: true }, (err, files) => {
+        if (err) {
+          console.log(err);
+        } else {
+          if (files.length === 0) return;
+          files.forEach((file) => {
+            const pathToFile = path.join(pathToFolder, file.name);
+            const pathToCopyFile = path.join(pathToCopyFolder, file.name);
+            copyFile(pathToFile, pathToCopyFile, (err) => {
+              if (err) console.log(err);
+            });
+          });
+        }
+      });
+    }
+  }
+});
