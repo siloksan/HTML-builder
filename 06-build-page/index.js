b520ce7c01710f20945ad6f89637515a5573c98f
siloksan <tutel07@gmail.com>
2024-01-17 21:18:23 +0300

    feat: finish build-page task

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -6,9 +6,6 @@ const {
   copyFile,
   stat,
   unlink,
-  rmdir,
-  access,
-  constants,
 } = require('fs');
 const path = require('path');
 
@@ -21,27 +18,32 @@ const pathToAssets = path.join(__dirname, 'assets');
 const buildHtml = path.join(pathToBuild, 'index.html');
 const buildCss = path.join(pathToBuild, 'style.css');
 
-// ------Создаём структуру папок билда-------
+// ------Create a folder structure for the build-------
+
 (async () => {
-  mkdir(pathToBuild, { recursive: true }, (err) => {
-    if (err) console.log(err);
-    return;
-  });
-  mkdir(pathToBuildAssets, { recursive: true }, (err) => {
-    if (err) console.log(err);
-    return;
-  });
-  const assets = await readdir(pathToAssets, { withFileTypes: true });
-  for (const asset of assets) {
-    const pathToAsset = path.join(pathToBuildAssets, asset.name);
-    mkdir(pathToAsset, { recursive: true }, (err) => {
+  try {
+    mkdir(pathToBuild, { recursive: true }, (err) => {
       if (err) console.log(err);
       return;
     });
+    mkdir(pathToBuildAssets, { recursive: true }, (err) => {
+      if (err) console.log(err);
+      return;
+    });
+    const assets = await readdir(pathToAssets, { withFileTypes: true });
+    for (const asset of assets) {
+      const pathToAsset = path.join(pathToBuildAssets, asset.name);
+      mkdir(pathToAsset, { recursive: true }, (err) => {
+        if (err) console.log(err);
+        return;
+      });
+    }
+  } catch (error) {
+    console.log('Folders are not created!', error);
   }
 })();
 
-// ---------------Удаляем все файлы в билде-----------
+//===============Delete all files in build============
 
 async function deleteFilesIntoDirectory(pathToFolder) {
   try {
@@ -53,13 +55,13 @@ async function deleteFilesIntoDirectory(pathToFolder) {
         if (err) return;
         else if (!stats.isDirectory()) {
           unlink(pathToSubject, (err) => {
-            if (err) console.log('удаление не удалось', err);
+            if (err) console.log('File is not deleted!', err);
           });
         }
       });
     }
   } catch (error) {
-    console.log('Folder is not deleted: ', pathToFolder, error);
+    console.log('Files into folder are not deleted! ', pathToFolder, error);
   }
 }
 
@@ -72,7 +74,7 @@ async function deleteFilesIntoDirectory(pathToFolder) {
   }
 })();
 
-//=============Создаём разметку==============================
+//=============Create the markup==============================
 
 const COMPONENTS = {};
 
@@ -102,182 +104,50 @@ const COMPONENTS = {};
     console.error('The markup could not be created:', err);
   }
 })();
-// ----------Собираем стили------------------
+
+//==============Collect all the styles===============
+
 (async () => {
-  const styles = await readdir(pathToStyles, { withFileTypes: true });
-  let content = '';
-  for (const style of styles) {
-    const extension = path.extname(style.name);
-    if (extension !== '.css') return;
-    const pathToStyle = path.join(pathToStyles, style.name);
-    const readableStream = createReadStream(pathToStyle, 'utf-8');
-    for await (const chunk of readableStream) {
-      content += chunk;
+  try {
+    const styles = await readdir(pathToStyles, { withFileTypes: true });
+    let content = '';
+    for (const style of styles) {
+      const extension = path.extname(style.name);
+      if (extension !== '.css') return;
+      const pathToStyle = path.join(pathToStyles, style.name);
+      const readableStream = createReadStream(pathToStyle, 'utf-8');
+      for await (const chunk of readableStream) {
+        content += chunk;
+      }
     }
+    const writeStream = createWriteStream(buildCss);
+    writeStream.write(content);
+  } catch (error) {
+    console.log('Styles have not been collected!', error);
   }
-  const writeStream = createWriteStream(buildCss);
-  writeStream.write(content);
 })();
-// -----------------------------------------
-// function folderExist(directoryPath) {
-//   return access(directoryPath, constants.F_OK, (err) => {
-//     if (err) {
-//       return;
-//     } else {
-//       return true;
-//     }
-//   });
-// }
-// ---------------------------------------------
-// folderExist(pathToAssets);
-// console.log('folderExist(pathToAssets): ', folderExist(pathToAssets));
 
-// stat(pathToBuild, (err, stats) => {
-//   if (err) {
-//     return;
-//   } else {
-//     if (stats.isDirectory()) {
-//       readdir(pathToBuild, { withFileTypes: true }, (err, files) => {
-//         if (err) {
-//           console.log(err);
-//         } else {
-//           if (files.length === 0) return;
-//           files.forEach((file) => {
-//             const pathToFile = path.join(pathToBuild, file.name);
-//             unlink(pathToFile, (err) => {
-//               if (err) console.log(err);
-//             });
-//           });
-//         }
-//       });
-//     }
-//   }
-// });
+//===========Copy all the assets=================================
 
-// async function deleteFilesIntoDirectory(pathToFolder) {
-//   const subjects = await readdir(pathToFolder, { withFileTypes: true });
-//   if (subjects.length === 0) {
-//     rmdir(pathToFolder, (err) => {
-//       if (err) throw err;
-//       console.log('Папка успешно удалена');
-//     });
-//     return;
-//   } else {
-//     for (const subject of subjects) {
-//       const pathToSubject = path.join(pathToFolder, subject.name);
-//       stat(pathToSubject, (err, stats) => {
-//         if (err) return;
-//         else if (stats.isDirectory()) {
-//           deleteFilesIntoDirectory(pathToSubject);
-//         } else {
-//           unlink(pathToSubject, (err) => {
-//             if (err) console.log('удаление не удалось', err);
-//           });
-//         }
-//       });
-//     }
-//   }
-// }
-
-// =============================================================
-// const ASSETS = {};
-
-// async function getStructureFolder(pathToFolder) {
-//   stat(pathToFolder, (err, stats) => {
-//     if (err) return;
-//     if (stats.isDirectory()) {
-//       const files = await readdir
-//     }
-//   });
-// }
-
-// (async () => {
-//   try {
-//     const folders = await readdir(pathToAssets, { withFileTypes: true });
-//     for (const folder of folders) {
-//       const pathToFile = path.join(pathToAssets, file.name);
-//       const pathToCopyFile = path.join(pathToBuild, file.name);
-//       copyFile(pathToFile, pathToCopyFile, (err) => {
-//         if (err) console.log(`The ${file} could not be copied: `, err);
-//       });
-//     }
-//   } catch (err) {
-//     console.error('The files could not be copied: ', err);
-//   }
-// })();
-
-// ----------------------------------------------
-
-// console.log('comp: ', COMPONENTS);
-// (async () => {
-//   try {
-//     console.log('arr: ', arr);
-//   } catch (err) {
-//     console.error(err);
-//   }
-// })();
-
-// async function readFilesIntoDirectory(pathToFolder, extension, object) {
-//   try {
-//     return await readdir(pathToFolder, { withFileTypes: true });
-//   } catch (error) {
-//     console.log(error);
-//   }
-// }
-
-// function saveContentInObj(pathToFolder, component, object) {
-//     const pathToFile = path.join(pathToFolder, component);
-//     const readableStream = createReadStream(pathToFile, 'utf-8');
-//     return new Promise((resolve, reject) => {
-//         let content = '';
-//     readableStream.on('data', (chunk) => {
-//       content += chunk;
-//     });
-//     readableStream.on('end', () => {
-//       object[component] = content;
-//       resolve();
-//     });
-//     readableStream.on('error', (err) => {
-//       reject(err);
-//     });
-//   });
-// }
-
-// function readFilesIntoDirectory(pathToFolder, extension, object) {
-//   return new Promise((resolve, reject) => {
-//     readdir(pathToFolder, { withFileTypes: true }, (err, files) => {
-//       if (err) {
-//         reject(err);
-//       } else {
-//         const promises = files.map((file) => {
-//           if (file.isFile() && path.extname(file.name) === extension) {
-//             return saveContentInObj(pathToFolder, file.name, object);
-//           }
-//         });
-//         Promise.all(promises)
-//           .then(() => {
-//             resolve();
-//           })
-//           .catch((err) => {
-//             reject(err);
-//           });
-//       }
-//     });
-//   });
-// }
-
-// readFilesIntoDirectory(pathToComponents, '.html', COMPONENTS);
-
-// console.log('COMPONENTS: ', COMPONENTS);
-// function getFileContent(pathToFolder, component) {
-//   const pathToFile = path.join(pathToFolder, component);
-//   let content = '';
-//   readFile(pathToFile, 'utf-8', (err, data) => {
-//     if (err) {
-//       console.log(err);
-//     } else {
-//       content = data;
-//     }
-//   });
-//   return content;
-// }
+(async () => {
+  try {
+    const folders = await readdir(pathToAssets, { withFileTypes: true });
+    for (const folder of folders) {
+      const pathToFolder = path.join(pathToAssets, folder.name);
+      const assets = await readdir(pathToFolder, { withFileTypes: true });
+      for (const asset of assets) {
+        const pathToAsset = path.join(pathToFolder, asset.name);
+        const pathToCopyAsset = path.join(
+          pathToBuildAssets,
+          folder.name,
+          asset.name,
+        );
+        copyFile(pathToAsset, pathToCopyAsset, (err) => {
+          if (err) console.log(`The ${asset.name} could not be copied: `, err);
+        });
+      }
+    }
+  } catch (err) {
+    console.error('The files could not be copied: ', err);
+  }
+})();
